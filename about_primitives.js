366b094eb233a352189360b599fc9b1e4878dd0e
Dan Garland <dan@dangarland.co.uk>
2013-10-06 13:38:08 +0100

    Starting with primitives

---
+++ about_primitives.js
@@ -0,0 +1,52 @@
+var ___ = "Fill me in!";
+
+exports.a_variable_that_has_never_been_defined_is_undefined = function(test) {
+	test.equal(___, typeof(some_made_up_variable));
+	test.done();
+};
+
+exports.undefined_is_a_primitive = function(test) {
+	test.equal(___, typeof(undefined));
+	test.done();
+};
+
+exports.a_variable_that_has_no_value_is_null = function(test) {
+	var b;
+	test.equal(___, b);
+	test.done();
+};
+
+exports.typeof_null_is_broken = function(test) {
+	test.equal(___, typeof(null), "This is not what we'd expect it to be...");
+	test.done();
+};
+
+exports.numbers_are_also_primitives = function(test) {
+	test.equal(___, typeof(3));
+	test.equal(___, typeof(3.14));
+	test.done();
+};
+
+exports.strings_are_also_primitives = function(test) {
+	test.equal(___, typeof("Hi"));
+	test.done();
+};
+
+exports.booleans_are_also_primitives = function(test) {
+	test.equal(___, typeof(true));
+	test.done();
+};
+
+exports.primitives_have_no_prototype_because_they_are_not_objects = function(test) {
+	var data_types = [undefined, null, true, 3, "hello"];
+
+	for(var primitive in data_types) {
+		try {
+			Object.getPrototypeOf(primitive);
+		}
+		catch(err) {
+			test.equal(___, err.name);
+		}
+	}
+	test.done();
+};
