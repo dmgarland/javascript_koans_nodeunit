211fe393db19a83f1aaceef14a2d2e3140de6f5f
Dan Garland <dan@dangarland.co.uk>
2014-04-20 12:40:00 +0100

    Adding prototype chain example

--- about_inheritance.js
+++ about_inheritance.js
@@ -35,8 +35,24 @@ Duck.prototype.speak = function() {
 
 var duck = new Duck("Donald");
 
-exports.test_inheritance_means_using_a_parent_object_as_the_prototype = function(test) {
-  test.equal(___, typeof(duck));
-  test.equal(___, Object.getPrototypeOf(duck) instanceof Animal);
+// exports.test_inheritance_means_using_a_parent_object_as_the_prototype = function(test) {
+//   test.equal(___, typeof(duck));
+//   test.equal(___, Object.getPrototypeOf(duck) instanceof Animal);
+//   test.done();
+// };
+
+// At this point, the 'prototype chain' of a Duck object is as follows:
+// Duck -> Animal -> Object -> Object.prototype -> null
+//
+exports.test_objects_ultimately_use_object_as_the_prototype = function(test) {
+  var duckPrototype = Object.getPrototypeOf(duck);
+  var animalPrototype = Object.getPrototypeOf(duckPrototype);
+  var objectPrototype = Object.getPrototypeOf(animalPrototype);
+  var noMorePrototypes = Object.getPrototypeOf(objectPrototype);
+
+  test.equal(___, duckPrototype instanceof Animal);
+  test.equal(___, animalPrototype instanceof Object);
+  test.equal(___, objectPrototype === Object.prototype);
+  test.equal(___, noMorePrototypes);
   test.done();
 };
